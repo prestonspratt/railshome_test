44ec1e4658ab68b161452ca4891ee06252b2d490
Preston Spratt <prestonspratt@gmail.com>
2012-12-12 19:30:20 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailshomeTest::Application.config.secret_token = 'c8542cfd21280e8967cbb0717ff14f8dde0ce74de2ab66289fe8a85e6a24f1f79402b940d438ce6b6f728bdaec073b5151c82641882d1b96cc923a56b00a403b'
